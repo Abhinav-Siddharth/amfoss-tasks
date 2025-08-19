e9b29e5015be7df3682ad47f820ae73198ebf6b6
Abhinav <abhinavsid16@gmail.com>
2025-08-19 12:24:14 +0530

    Setup for user profile

---
+++ TimeTickQuiz-2/src/user_profile.py
@@ -0,0 +1,23 @@
+import json
+import os
+
+PROFILES_FILE = os.path.join(os.path.dirname(__file__), "..", "profiles.json")
+
+def load_profiles():
+    if not os.path.exists(PROFILES_FILE):
+        return {}
+    with open(PROFILES_FILE, "r") as f:
+        return json.load(f)
+
+def save_profiles(profiles):
+    with open(PROFILES_FILE, "w") as f:
+        json.dump(profiles, f, indent=4)
+
+def update_score(username, score):
+    profiles = load_profiles()
+    profiles[username] = profiles.get(username, 0) + score
+    save_profiles(profiles)
+
+def get_score(username):
+    profiles = load_profiles()
+    return profiles.get(username, 0)
